ffc685f5cf2f08547b5a9d1aa6548da5a640c2a5
arangrhie <arrhie@gmail.com>
2020-02-06 00:54:13 -0500

    plot block contig scaffold N and NG stats

---
+++ plot/plot_block_N.R
@@ -0,0 +1,144 @@
+#!/usr/bin/env Rscript
+
+require("argparse")
+require("ggplot2")
+require("scales")
+
+parser <- ArgumentParser(description = "Make block N* or NG* plots. Applicable for scaffolds, contigs, and phased blocks.")
+parser$add_argument("-b", "--block", type="character", help="sorted .sizes file", default=NULL)
+parser$add_argument("-s", "--scaff", type="character", help="sorted .sizes file", default=NULL)
+parser$add_argument("-c", "--contig", type="character", help="sorted .sizes file", default=NULL)
+parser$add_argument("-o", "--out", type="character", help="output prefix (required) [default %(default)s]", default="out")
+parser$add_argument("-g", "--gsize", type="integer", default=0, help="genome size for computing NG* (optional)")
+parser$add_argument("-x", "--xdim", type="double", default=6, help="width of plot [default %(default)s]")
+parser$add_argument("-y", "--ydim", type="double", default=5, help="height of plot [default %(default)s]")
+parser$add_argument("-p", "--pdf", dest='pdf', default=FALSE, action='store_true', help="set to get output in .pdf. [default .png]")
+args <- parser$parse_args()
+
+fancy_scientific <- function(d) {
+  # turn in to character string in scientific notation
+  d <- format(d, scientific = TRUE)
+  # quote the part before the exponent to keep all the digits and turn the 'e+' into 10^ format
+  d <- gsub("^(.*)e\\+", "'\\1'%*%10^", d)
+  # convert 0x10^00 to 0
+  d <- gsub("\\'0[\\.0]*\\'(.*)", "'0'", d)
+  # return this as an expression
+  parse(text=d)
+}
+
+save_plot <- function(name, type, stats, outformat, h, w) {
+  ggsave(file = paste(name, type, stats, outformat, sep = "."), height = h, width = w)
+}
+
+attach_n <- function(dat, gsize=0) {
+  dat = read.table(dat, header = F)
+  names(dat) = c("Type", "Group", "Size")
+  dat$Sum = cumsum(dat$Size)
+  
+  if (gsize == 0) {
+    # N*
+    gsize = sum(dat$Size)
+  }
+  dat$N = 100*dat$Sum/gsize
+  dat$N2 = 100*c(0, dat$Sum[-nrow(dat)]/gsize)
+  return(dat)
+}
+
+get_dummy <- function(dat=NULL, type) {
+  x_max=max(dat$N)
+  data.frame(Type = c(type), Group = c("dummy"), Size = c(0), Sum = c(0), N = c(x_max), N2 = c(x_max))
+}
+
+bind_blocks <- function(block, block_dummy, scaff, scaff_dummy, contig, contig_dummy) {
+  
+  blocks = data.frame()
+  if (!is.null(block)) {
+    blocks=rbind(blocks, block, block_dummy)
+  }
+  if (!is.null(scaff)) {
+    blocks=rbind(blocks, scaff, scaff_dummy)
+  }
+  if (!is.null(contig)) {
+    blocks=rbind(blocks, contig, contig_dummy)
+  }
+  return(blocks)
+}
+
+plot_block <- function(dat = NULL, stats) {
+  # by phased block
+  y_max=max(dat$Size)
+  ggplot(data = dat, aes(x = dat[,5], y = dat[,3], fill = dat[,2], colour = dat[,2])) +
+    geom_rect(xmin=dat[,6], xmax=dat[,5], ymin=0, ymax=dat[,3], alpha=0.7) +
+    theme_bw() +
+    theme(legend.text = element_text(size=11),
+          legend.position = c(0.95,0.95),  # Modify this if the legend is covering your favorite circle
+          legend.background = element_rect(size=0.1, linetype="solid", colour ="black"),
+          legend.box.just = "right",
+          legend.justification = c("right", "top"),
+          axis.title=element_text(size=14,face="bold"),
+          axis.text=element_text(size=12)) +
+    scale_fill_brewer(palette = "Set1", name= names(dat)[2]) +
+    scale_colour_brewer(palette = "Set1", name= names(dat)[2]) +
+    scale_x_continuous(limits = c(0, 100)) +
+    scale_y_continuous(limits = c(0, y_max), labels = fancy_scientific) +
+    xlab(stats) + ylab("Size (bp)") +
+    geom_vline(xintercept = 50, show.legend = FALSE, linetype="dashed", color="black")
+}
+
+block_n <- function(block=NULL, scaff=NULL, contig=NULL, out, gsize = 0, w = 6, h = 5, pdf=FALSE) {
+  
+  outformat="png"
+  if (pdf) {
+    outformat="pdf"
+  }
+  
+  # Read file
+  if (!is.null(block)) {
+    block = attach_n(dat = block, gsize = gsize)
+    block_dummy = get_dummy(dat = block, type = "block")
+  }
+  
+  if (!is.null(scaff)) {
+    scaff = attach_n(dat = scaff, gsize = gsize)
+    scaff_dummy = get_dummy(dat = scaff, type = "scaffold")
+  }
+  
+  if (!is.null(contig)) {
+    contig = attach_n(dat = contig, gsize = gsize)
+    contig_dummy = get_dummy(dat = contig, type = "contig")
+  }
+  
+  stats = "NG"
+  if (gsize == 0) {
+    stats = "N"
+  }
+
+  # Plot phase blocks filled by haplotypes
+  plot_block(block, stats)
+  save_plot(out, "block", stats, outformat, h = h, w = w)
+  
+  dat = bind_blocks(block, block_dummy, scaff, scaff_dummy, contig, contig_dummy)
+  y_max=max(dat$Size)
+  
+  ggplot(data = dat, aes(x = N2, y = Size, colour = Type)) +
+    geom_step() +
+    theme_bw() +
+    theme(legend.text = element_text(size=11),
+          legend.position = c(0.95,0.95),  # Modify this if the legend is covering your favorite circle
+          legend.background = element_rect(size=0.1, linetype="solid", colour ="black"),
+          legend.box.just = "right",
+          legend.justification = c("right", "top"),
+          axis.title=element_text(size=14,face="bold"),
+          axis.text=element_text(size=12)) +
+    scale_fill_brewer(palette = "Set1", name = "Type") +
+    scale_colour_brewer(palette = "Set1", name= "Type") +
+    scale_x_continuous(limits = c(0, 100)) +
+    scale_y_continuous(limits = c(0, y_max), labels = fancy_scientific) +
+    xlab(stats) + ylab("Size (bp)") +
+    geom_vline(xintercept = 50, show.legend = FALSE, linetype="dashed", color="black")
+  
+  save_plot(out, "continuity", stats, outformat, h = h, w = w)
+}
+
+block_n(block = args$block, scaff = args$scaff, contig = args$contig, out = args$out, gsize = args$gsize, w = args$xdim, h = args$ydim, pdf = args$pdf)
+
---
+++ trio/block_n_stats.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+
+
+if [[ "$#" -lt 3 ]]; then
+    echo "Usage: ./block_n_stats.sh <asm1.fasta> <asm1.*.phased_block.bed> [<asm2.fasta> <asm2.*.phased_block.bed>] <out> [genome_size]"
+    echo
+    echo -e "\t<asm1.fasta>:\tAssembly 1 fasta file"
+    echo -e "\t<*.asm1.*.phased_block.bed>:\tAssembly 1 phased block .bed file"
+    echo
+    echo -e "\t<asm2.fasta>:\tAssembly 2 fasta file (optional)"
+    echo -e "\t<*.asm2.*.phased_block.bed>:\tAssembly 1 phased block .bed file (required when <asm2.fasta> is provided)"
+    echo
+    echo -e "\t<out>:\tOutput prefix"
+    echo -e "\t[genome_size]:\tEstimated genome size (bp)"
+    exit -1
+fi
+
+asm1=$1
+asm1=${asm1/.fasta/}
+block1=$2
+
+asm2=""
+if [[ "$#" -gt 4 ]]; then
+    asm2=$3
+    asm2=${asm2/.fasta/}
+    block2=$4
+    out=$5
+    g_size=$6
+else
+    out=$3
+    g_size=$4
+fi
+
+module load bedtools
+module load samtools
+module load R
+
+for asm in $asm1 $asm2
+do
+    if [[ ! -e $asm.fasta.fai ]]; then
+        echo "# Generate $asm.fasta.fai"
+        samtools faidx $asm.fasta
+    else
+        echo "*** # Found $asm.fasta.fai ***"
+    fi
+    echo
+
+    if [[ ! -e $asm.gaps.bed ]]; then
+        echo "# Get gaps"
+        java -jar -Xmx4g $MERQURY/trio/fastaGetGaps.jar $asm.fasta $asm.gaps
+        awk -F "\t" '{print $1"\t"$2"\t"$3"\tgap"}' $asm.gaps > $asm.gaps.bed
+    else
+        echo "*** # Found $asm.gaps.bed ***"
+    fi
+    echo
+    
+    num_gaps=`wc -l $asm.gaps.bed | awk '{print $1}'`
+    if [[ $num_gaps -gt 0 ]]; then
+        echo "# Found $num_gaps. Generating stats for both scaffolds and contigs."
+        awk -v asm=$asm '{print "scaffold\t"asm"\t"$2}' $asm.fasta.fai | sort -nr -k3 - > $out.$asm.scaff.sizes
+        awk '{print $1"\t0\t"$2}' $asm.fasta.fai | bedtools subtract -a - -b $asm.gaps.bed | awk '{print "contig\t"asm"\t"($NF-$(NF-1))}' | sort -nr -k3 - > $out.$asm.contigs.sizes
+    else
+        echo "# No gaps found. This is a contig set."
+        awk -v asm=$asm '{print "contig\t"asm"\t"$2}' $asm.fasta.fai | sort -nr -k3 - > $out.$asm.contig.sizes
+    fi
+    echo
+done
+
+block1=`ls $block1`
+echo "# Convert $block1 to sizes"
+awk -v asm="block" -F "\t" '{print asm"\t"$4"\t"($3-$2)}' $block1 | sort -nr -k3 - > ${block1/.bed/.sizes}
+echo " Result saved as ${block1/.bed/.sizes}"
+echo
+
+if [[ -s $out.$asm1.scaff.sizes ]]; then
+    scaff="-s $out.$asm1.scaff.sizes"
+fi
+
+if [[ "$g_size" != "" ]]; then
+    g_size="-g $g_size"
+fi
+
+echo "# Plot $block1"
+echo "\
+Rscript $MERQURY/plot/plot_block_N.R -b ${block1/.bed/.sizes} -c $out.$asm1.contig.sizes $scaff -o $out.$asm1 $g_size"
+Rscript $MERQURY/plot/plot_block_N.R -b ${block1/.bed/.sizes} -c $out.$asm1.contig.sizes $scaff -o $out.$asm1 $g_size
+echo
+
+if [[ "$block2" == "" ]]; then
+    echo "# No block2 found. Done!"
+    exit 0
+fi
+
+block2=`ls $block2`
+echo "# Convert $block2 to sizes"
+awk -v asm="block" -F "\t" '{print asm"\t"$4"\t"($3-$2)}' $block2 | sort -nr -k3 > ${block2/.bed/.sizes}
+echo " Result saved as ${block2/.bed/.sizes}"
+echo
+
+if [[ -s $out.$asm2.scaff.sizes ]]; then
+    scaff="-s $out.$asm2.scaff.sizes"
+fi
+
+echo "# Plot $block2"
+echo "\
+Rscript $MERQURY/plot/plot_block_N.R -b ${block2/.bed/.sizes} -c $out.$asm2.contig.sizes $scaff -o $out.$asm2 $g_size"
+Rscript $MERQURY/plot/plot_block_N.R -b ${block2/.bed/.sizes} -c $out.$asm2.contig.sizes $scaff -o $out.$asm2 $g_size
+echo
+
