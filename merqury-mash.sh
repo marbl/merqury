012b4d9bf889821b680e8a3f84dacca7082fa5b3
arangrhie <arrhie@gmail.com>
2020-03-05 17:41:10 -0500

    add QV estimates from mash

---
+++ merqury-mash.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ "$#" -lt 4 ]]; then
+echo "Usage: ./merqury-mash.sh <k-size> <asm.fasta> <input_fofn> <cpus>"
+echo
+echo "This is the fast version of QV estimates using mash."
+echo "The QVs are not completely agreeing with the one obtained with the full merqury run,"
+echo "but is more designed to help give a quick estimate of the assembly QV."
+exit 0
+fi
+
+k=$1
+asm=$2
+input_fofn=$3
+cpus=$4
+
+name=`echo $asm | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz//g' | sed 's/.fa.gz//g'`
+module load mash
+
+mash sketch -s 1000000 -k $k $asm
+mash screen -p $cpus $asm.msh `cat $input_fofn | tr '\n' ' '` > $name.msh.idy
+cat $name.msh.idy  | awk -v name=$name '{print name"\t"$2"\t"-10*log(1-$1)/log(10)"\t"(1-$1)}' | tr '/' '\t' > $name.msh.qv
+
+
