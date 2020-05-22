17c04983b10b5e91915a0a5602af500d5dd46c6b
arangrhie <arrhie@gmail.com>
2020-05-22 19:51:52 -0400

    convert bed to bigWig

---
+++ util/bed_to_bigwig.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+if [[ -z $1 ]]; then
+    echo "Usage: ./bed_to_bigWig.sh <bed> <asm.fai> <convert_pipes>"
+    echo -e "\t<bed>: ex. out.asm.mat.inherited.bed"
+    echo -e "\t<asm.fai>: generate with samtools faidx"
+    echo -e "\t<convert_pipes>: convert `|` to `_` by default. Set to F if not wanted."
+    exit 0
+fi
+
+bed=$1
+fai=$2
+convert=$3
+
+module load bedtools
+module load ucsc/396
+
+bg=${bed/.bed/.bg}
+
+if [[ "$convert" = "F" ]]; then
+    echo "# keep pipes"
+    sizes=${fai/.fai/.sizes}
+    cat $fai | cut -f1-2 > $sizes
+    cat $bed | bedtools genomecov -bg -g $sizes -i - > $bg
+else
+    sizes=${fai/.fai/.sizes}
+    cat $fai | cut -f1-2 | sed 's/|/_/g' > $sizes
+    cat $bed | sed 's/|/_/g' | bedtools genomecov -bg -g $sizes -i - > $bg
+fi
+
+sort=${bg/.bg/.srt.bg}
+bedSort $bg $sort
+
+bw=${bed/.bed/.bigwig}
+bedGraphToBigWig $sort $sizes $bw
+
+rm $bg $sort
+
+
