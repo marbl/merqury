77fe7c1cfa3c9acc79c0097a9b67789b0b33833f
arangrhie <arrhie@gmail.com>
2020-06-30 11:56:31 -0400

    get asm.meryl as input

--- eval/asm_multiplicity.sh
+++ eval/asm_multiplicity.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 if [[ "$#" -lt 2 ]]; then
-	echo "Usage: ./asm_multiplicity.sh <asm.fasta> <out>"
+	echo "Usage: ./asm_multiplicity.sh <asm.fasta> <asm.meryl> <out>"
 	echo -e "\t<asm.fasta>: assembly fasta file"
+	echo -e "\t<asm.meryl>: assembly meryl dir"
 	echo -e "\t<out>: output file prefix. <out>.copies.wig and <out>.asm_multiplicity.bigWig will be generated."
 	exit 0
 fi
 
 asm_fa=$1
-out=$2
-asm=`echo $asm_fa | sed 's/.gz$//g' | sed 's/.fasta$//g' | sed 's/.fa$//g'`
+asm=$2
+out=$3
 
 # Requirements: samtools, ucsc kent utils
 module load samtools
@@ -19,9 +20,10 @@ if [[ ! -e $asm_fa ]]; then
 	samtools faidx $asm_fa
 fi
 
+
 echo "
 # Collect copy numbers in assembly"
-meryl-lookup -dump -memory 12 -sequence $asm_fa -mers $asm.meryl | awk '$4=="T"' | java -jar -Xmx1g $MERQURY/util/merylDumpToWig.jar - > $out.copies.wig
+meryl-lookup -dump -memory 12 -sequence $asm_fa -mers $asm | awk '$4=="T"' | java -jar -Xmx1g $MERQURY/util/merylDumpToWig.jar - > $out.copies.wig
 
 echo "
 # Convert to bigwig"
