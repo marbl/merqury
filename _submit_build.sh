b7d6874e58e3dd4215de63801346b8d350a38302
arangrhie <arrhie@gmail.com>
2020-11-07 16:38:28 -0500

    re format

--- _submit_build.sh
+++ _submit_build.sh
@@ -3,13 +3,13 @@
 build=$MERQURY/build
 
 if [ -z $1 ]; then
-    echo "Usage: ./_submit_meryl2_build.sh <k-size> <input.fofn> <out_prefix> [mem=T]"
-    echo -e "\t<k-size>: kmer size k"
-    echo -e "\t<input.fofn>: ls *.fastq.gz > input.fofn. include both R1 and R2 for paired-end sequencing."
-    echo -e "\t\taccepts fasta, fastq, gzipped or not."
-    echo -e "\t<out_prefix>: Final merged meryl db will be named as <out_prefix>.meryl"
-    echo -e "\t[mem=T]: Submit memory option on sbatch [DEFAULT=TRUE]. Set it to F to turn it off."
-    exit 0
+  echo "Usage: ./_submit_meryl2_build.sh <k-size> <input.fofn> <out_prefix> [mem=T]"
+  echo -e "\t<k-size>: kmer size k"
+  echo -e "\t<input.fofn>: ls *.fastq.gz > input.fofn. include both R1 and R2 for paired-end sequencing."
+  echo -e "\t\taccepts fasta, fastq, gzipped or not."
+  echo -e "\t<out_prefix>: Final merged meryl db will be named as <out_prefix>.meryl"
+  echo -e "\t[mem=T]: Submit memory option on sbatch [DEFAULT=TRUE]. Set it to F to turn it off."
+  exit 0
 fi
 
 k=$1
@@ -25,9 +25,9 @@ mkdir -p logs
 # Split files when >12GB
 cpus=20
 if [[ "$mem_opt" = "F" ]]; then
-        mem=""
+  mem=""
 else
-        mem="--mem=4g"
+  mem="--mem=4g"
 fi
 name=$out_prefix.split
 script=$build/split.sh
@@ -43,50 +43,50 @@ split_arrs=""
 
 for i in $(seq 1 $LEN)
 do
-	fq=`sed -n ${i}p $input_fofn`
-	GB=`du -k $fq  | awk '{printf "%.0f", $1/1024/1024}'`
-	if [[ $GB -lt 15 ]]; then
-	    echo "$fq is $GB, less than 15GB. Skip splitting."
-	    echo $fq >> $input_fofn.$i
+  fq=`sed -n ${i}p $input_fofn`
+  GB=`du -k $fq  | awk '{printf "%.0f", $1/1024/1024}'`
+  if [[ $GB -lt 15 ]]; then
+    echo "$fq is $GB, less than 15GB. Skip splitting."
+	  echo $fq >> $input_fofn.$i
 	else
-	    echo "$fq is $GB, over 15GB. Will split and run meryl in parallel."
-	    echo "Split files will be in $input_fofn.$i"
-	    args="$input_fofn"
-	    split_arrs="$split_arrs$i,"
-	    wait_for="${wait_for}afterok:$split_jid,"
-	    split=1
-	    echo
+	  echo "$fq is $GB, over 15GB. Will split and run meryl in parallel."
+	  echo "Split files will be in $input_fofn.$i"
+	  args="$input_fofn"
+	  split_arrs="$split_arrs$i,"
+	  wait_for="${wait_for}afterok:$split_jid,"
+	  split=1
+	  echo
 	fi
 done
 
 if [ $split -eq 1 ]; then
-	split_arrs=${split_arrs%,}
-	echo "
-	sbatch -D $path -J $name --array=$split_arrs --partition=$partition $mem --cpus-per-task=$cpus --time=$walltime --error=$log --output=$log $script $args"
-        sbatch -D $path -J $name --array=$split_arrs --partition=$partition $mem --cpus-per-task=$cpus --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' > split_jid
-	split_jid=`cat split_jid`
-	wait_for="afterok:$split_jid"
-	cpus=2
-	if [[ "$mem_opt" = "F" ]]; then
-	        mem=""
-	else
-        	mem="--mem=1g"
-	fi
+  split_arrs=${split_arrs%,}
+  echo "
+  sbatch -D $path -J $name --array=$split_arrs --partition=$partition $mem --cpus-per-task=$cpus --time=$walltime --error=$log --output=$log $script $args"
+  sbatch -D $path -J $name --array=$split_arrs --partition=$partition $mem --cpus-per-task=$cpus --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' > split_jid
+  split_jid=`cat split_jid`
+  wait_for="afterok:$split_jid"
+  cpus=2
+  if [[ "$mem_opt" = "F" ]]; then
+    mem=""
+  else
+    mem="--mem=1g"
+  fi
 	name=$out_prefix.concat
 	script=$build/concat_splits.sh
 	args="$k $input_fofn $out_prefix"
 	partition=quick
 	walltime=10:00
 	path=`pwd`
-	log=logs/$name.%A.log
-	wait_for="--dependency=${wait_for%,}"
-	echo "
-	sbatch -D $path -J $name --partition=$partition $mem --cpus-per-task=$cpus --time=$walltime $wait_for --error=$log --output=$log $script $args"
-	sbatch -D $path -J $name --partition=$partition $mem --cpus-per-task=$cpus --time=$walltime $wait_for --error=$log --output=$log $script $args
-	exit 0
+  log=logs/$name.%A.log
+  wait_for="--dependency=${wait_for%,}"
+  echo "
+  sbatch -D $path -J $name --partition=$partition $mem --cpus-per-task=$cpus --time=$walltime $wait_for --error=$log --output=$log $script $args"
+  sbatch -D $path -J $name --partition=$partition $mem --cpus-per-task=$cpus --time=$walltime $wait_for --error=$log --output=$log $script $args
+  exit 0
 else
-	rm $input_fofn.*
-	wait_for=""
+  rm $input_fofn.*
+  wait_for=""
 fi
 
 offset=$((LEN/1000))
@@ -94,9 +94,9 @@ leftovers=$((LEN%1000))
 
 cpus=32 # Max: 64 per each .meryl/ file writer
 if [[ "$mem_opt" = "F" ]]; then
-	mem=""
+  mem=""
 else
-	mem="--mem=40g"
+  mem="--mem=40g"
 fi
 name=$out_prefix.count
 script=$build/count.sh
@@ -106,22 +106,22 @@ path=`pwd`
 log=logs/$name.%A_%a.log
 
 if [ -e meryl_count.jid ]; then
-    echo "Removing meryl_count.jid"
-    cat meryl_count.jid
-    rm meryl_count.jid
+  echo "Removing meryl_count.jid"
+  cat meryl_count.jid
+  rm meryl_count.jid
 fi
 
 for i in $(seq 0 $offset)
 do
-    args="$k $input_fofn $i"
-    if [[ $i -eq $offset ]]; then
-        arr_max=$leftovers
-    else
-        arr_max=1000
-    fi
-    echo "\
-    sbatch -J $name $mem --partition=$partition --cpus-per-task=$cpus -D $path --array=1-$arr_max --time=$walltime --error=$log --output=$log $script $args"
-    sbatch -J $name $mem --partition=$partition --cpus-per-task=$cpus -D $path --array=1-$arr_max --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' >> meryl_count.jid
+  args="$k $input_fofn $i"
+  if [[ $i -eq $offset ]]; then
+    arr_max=$leftovers
+  else
+    arr_max=1000
+  fi
+  echo "\
+  sbatch -J $name $mem --partition=$partition --cpus-per-task=$cpus -D $path --array=1-$arr_max --time=$walltime --error=$log --output=$log $script $args"
+  sbatch -J $name $mem --partition=$partition --cpus-per-task=$cpus -D $path --array=1-$arr_max --time=$walltime --error=$log --output=$log $script $args | awk '{print $NF}' >> meryl_count.jid
 done
 
 # Wait for these jobs
@@ -130,24 +130,24 @@ WAIT=${WAIT%,}
 
 ## Collect .meryl list
 if [ -e  meryl_count.meryl.list ]; then
-    echo "Removing meryl_count.meryl.list"
-    cat meryl_count.meryl.list
-    rm  meryl_count.meryl.list
+  echo "Removing meryl_count.meryl.list"
+  cat meryl_count.meryl.list
+  rm  meryl_count.meryl.list
 fi
 
 for line_num in $(seq 1 $LEN)
 do
-    input=`sed -n ${line_num}p $input_fofn`
-    name=`echo $input | sed 's/.fastq.gz$//g' | sed 's/.fq.gz$//g' | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
-    name=`basename $name`
-    echo "$name.$k.$line_num.meryl" >> meryl_count.meryl.list
+  input=`sed -n ${line_num}p $input_fofn`
+  name=`echo $input | sed 's/.fastq.gz$//g' | sed 's/.fq.gz$//g' | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
+  name=`basename $name`
+  echo "$name.$k.$line_num.meryl" >> meryl_count.meryl.list
 done
 
 cpus=48 # Max: 64 per each .meryl/ file writer
 if [[ "$mem_opt" = "F" ]]; then
-        mem=""
+  mem=""
 else
-        mem="--mem=32g"
+  mem="--mem=32g"
 fi
 walltime=4:00:00
 partition=norm
--- eval/spectra-cn.sh
+++ eval/spectra-cn.sh
@@ -11,8 +11,8 @@ echo
 
 
 if [[ $# -lt 3 ]]; then
-    echo "No args provided. Exit."
-    exit -1
+  echo "No args provided. Exit."
+  exit -1
 fi
 
 source $MERQURY/util/util.sh
@@ -26,35 +26,35 @@ echo "Detected k-mer size $k"
 echo
 
 if [ -z $name ]; then
-	name=$3
-	asm2_fa=""
+  name=$3
+  asm2_fa=""
 else
-	asm2_fa=`link $3`
+  asm2_fa=`link $3`
 fi
 
 if [ -s $name ]; then
-        echo "$name already exists. Provide a different name."
-        exit -1
+  echo "$name already exists. Provide a different name."
+  exit -1
 fi
 
 
 has_module=$(check_module)
 if [[ $has_module -gt 0 ]]; then
-	echo "No modules available.."
+  echo "No modules available.."
 else
-	module load R
+  module load R
 fi
 
 
-asm1=`echo $asm1_fa | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
+asm1=`echo $asm1_fa | sed 's/.fa$//g' | sed 's/.fasta$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
 
 echo "# Get solid k-mers"
 
 if [ ! -s ${read/.meryl/.filt} ]; then
-	$MERQURY/build/filt.sh $read
+  $MERQURY/build/filt.sh $read
 else
-	echo "*** Found ${read/.meryl/.filt}. ***"
-	echo
+  echo "*** Found ${read/.meryl/.filt}. ***"
+  echo
 fi
 filt=`cat ${read/.meryl/.filt}`
 read_solid=${read/.meryl/}.gt$filt.meryl
@@ -63,134 +63,134 @@ echo "=== Generate spectra-cn plots per assemblies and get QV, k-mer completenes
 echo
 for asm_fa in $asm1_fa $asm2_fa	# will generate only for asm1_fa if asm2_fa is empty
 do
-	asm=`echo $asm_fa | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
+  asm=`echo $asm_fa | sed 's/.fa$//g' | sed 's/.fasta$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
 
-	if [ ! -e $asm.meryl ]; then
-	    echo "# Generate meryl db for $asm"
-	    meryl count k=$k output ${asm}.meryl $asm_fa
-	    echo
-	fi
+  if [ ! -e $asm.meryl ]; then
+    echo "# Generate meryl db for $asm"
+    meryl count k=$k output ${asm}.meryl $asm_fa
+    echo
+  fi
 
-	echo "# Collect read counts per asm copies"
-	hist=$name.$asm.spectra-cn.hist
-	hist_asm_only=$name.$asm.only.hist
+  echo "# Collect read counts per asm copies"
+  hist=$name.$asm.spectra-cn.hist
+  hist_asm_only=$name.$asm.only.hist
 
-	if [[ -s $hist ]]; then
-		echo
-		echo "*** $hist found. ***"
-		echo
-	else
+  if [[ -s $hist ]]; then
+    echo
+    echo "*** $hist found. ***"
+    echo
+  else
 
-		echo -e "Copies\tkmer_multiplicity\tCount" > $hist
+    echo -e "Copies\tkmer_multiplicity\tCount" > $hist
 
-		echo "# Read only"
-		meryl difference output read.k$k.$asm.0.meryl $read $asm.meryl
-		meryl histogram read.k$k.$asm.0.meryl | awk '{print "read-only\t"$0}' >> $hist
+    echo "# Read only"
+    meryl difference output read.k$k.$asm.0.meryl $read $asm.meryl
+    meryl histogram read.k$k.$asm.0.meryl | awk '{print "read-only\t"$0}' >> $hist
 
-		echo "# Copy 1 ~ 4"
-		for i in $(seq 1 4)
-		do
-		    echo "Copy = $i .."
-		    meryl intersect output read.k$k.$asm.$i.meryl $read [ equal-to $i ${asm}.meryl ]
-		    meryl histogram read.k$k.$asm.$i.meryl | awk -v cn=$i '{print cn"\t"$0}' >> $hist
-		    rm -r read.k$k.$asm.$i.meryl
-		    echo
-		done
+    echo "# Copy 1 ~ 4"
+    for i in $(seq 1 4)
+    do
+      echo "Copy = $i .."
+      meryl intersect output read.k$k.$asm.$i.meryl $read [ equal-to $i ${asm}.meryl ]
+      meryl histogram read.k$k.$asm.$i.meryl | awk -v cn=$i '{print cn"\t"$0}' >> $hist
+      rm -r read.k$k.$asm.$i.meryl
+      echo
+    done
 
-		echo "Copy >4 .."
-		meryl intersect output read.k$k.$asm.gt$i.meryl $read [ greater-than $i ${asm}.meryl ]
-		meryl histogram read.k$k.$asm.gt$i.meryl | awk -v cn=">$i" '{print cn"\t"$0}' >> $hist
-		rm -r read.k$k.$asm.gt$i.meryl
-		echo
-	fi
+    echo "Copy >4 .."
+    meryl intersect output read.k$k.$asm.gt$i.meryl $read [ greater-than $i ${asm}.meryl ]
+    meryl histogram read.k$k.$asm.gt$i.meryl | awk -v cn=">$i" '{print cn"\t"$0}' >> $hist
+    rm -r read.k$k.$asm.gt$i.meryl
+    echo
+  fi
 
-	echo "# Copy numbers in k-mers found only in asm"
-	meryl difference output $asm.0.meryl ${asm}.meryl $read
-	PRESENT=`meryl statistics ${asm}.0.meryl  | head -n4 | tail -n1 | awk '{print $2}'`
-	DISTINCT=`meryl statistics ${asm}.0.meryl  | head -n3 | tail -n1 | awk '{print $2}'`
-	MULTI=$(($PRESENT-$DISTINCT))
-	echo -e "1\t0\t$DISTINCT" > $hist_asm_only
-	echo -e "2\t0\t$MULTI" >> $hist_asm_only
-	echo
+  echo "# Copy numbers in k-mers found only in asm"
+  meryl difference output $asm.0.meryl ${asm}.meryl $read
+  PRESENT=`meryl statistics ${asm}.0.meryl  | head -n4 | tail -n1 | awk '{print $2}'`
+  DISTINCT=`meryl statistics ${asm}.0.meryl  | head -n3 | tail -n1 | awk '{print $2}'`
+  MULTI=$(($PRESENT-$DISTINCT))
+  echo -e "1\t0\t$DISTINCT" > $hist_asm_only
+  echo -e "2\t0\t$MULTI" >> $hist_asm_only
+  echo
 
-	echo "# Plot $hist"
-	echo "\
-	Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.$asm.spectra-cn -z $hist_asm_only"
-	Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.$asm.spectra-cn -z $hist_asm_only
-	echo
+  echo "# Plot $hist"
+  echo "\
+  Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.$asm.spectra-cn -z $hist_asm_only"
+  Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.$asm.spectra-cn -z $hist_asm_only
+  echo
 
-	echo "# QV statistics"
-	ASM_ONLY=`meryl statistics ${asm}.0.meryl  | head -n4 | tail -n1 | awk '{print $2}'`
-        TOTAL=`meryl statistics ${asm}.meryl  | head -n4 | tail -n1 | awk '{print $2}'`
-        ERROR=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (1-(1-$1/$2)^(1/k))}'`
-        QV=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (-10*log(1-(1-$1/$2)^(1/k))/log(10))}'`
-        echo -e "$asm\t$ASM_ONLY\t$TOTAL\t$QV\t$ERROR" >> $name.qv
-	echo
+  echo "# QV statistics"
+  ASM_ONLY=`meryl statistics ${asm}.0.meryl  | head -n4 | tail -n1 | awk '{print $2}'`
+  TOTAL=`meryl statistics ${asm}.meryl  | head -n4 | tail -n1 | awk '{print $2}'`
+  ERROR=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (1-(1-$1/$2)^(1/k))}'`
+  QV=`echo "$ASM_ONLY $TOTAL" | awk -v k=$k '{print (-10*log(1-(1-$1/$2)^(1/k))/log(10))}'`
+  echo -e "$asm\t$ASM_ONLY\t$TOTAL\t$QV\t$ERROR" >> $name.qv
+  echo
 
-	echo "# Per seq QV statistics"
-	meryl-lookup -existence -sequence $asm_fa -mers $asm.0.meryl/ | \
-	awk -v k=$k '{print $1"\t"$NF"\t"$(NF-2)"\t"(-10*log(1-(1-$NF/$(NF-2))^(1/k))/log(10))"\t"(1-(1-$NF/$(NF-2))^(1/k))}' > $name.$asm.qv
-	echo
+  echo "# Per seq QV statistics"
+  meryl-lookup -existence -sequence $asm_fa -mers $asm.0.meryl/ | \
+  awk -v k=$k '{print $1"\t"$NF"\t"$(NF-2)"\t"(-10*log(1-(1-$NF/$(NF-2))^(1/k))/log(10))"\t"(1-(1-$NF/$(NF-2))^(1/k))}' > $name.$asm.qv
+  echo
 
-        echo "# k-mer completeness (recoveray rate) with solid k-mers for $asm with > $filt counts"
-	meryl intersect output $asm.solid.meryl $asm.meryl $read_solid
-        TOTAL=`meryl statistics $read_solid | head -n3 | tail -n1 | awk '{print $2}'`
-        ASM=`meryl statistics $asm.solid.meryl | head -n3 | tail -n1 | awk '{print $2}'`
-        echo -e "${asm}\tall\t${ASM}\t${TOTAL}" | awk '{print $0"\t"((100*$3)/$4)}' >> completeness.stats
-	rm -r $asm.solid.meryl
-	echo
+  echo "# k-mer completeness (recoveray rate) with solid k-mers for $asm with > $filt counts"
+  meryl intersect output $asm.solid.meryl $asm.meryl $read_solid
+  TOTAL=`meryl statistics $read_solid | head -n3 | tail -n1 | awk '{print $2}'`
+  ASM=`meryl statistics $asm.solid.meryl | head -n3 | tail -n1 | awk '{print $2}'`
+  echo -e "${asm}\tall\t${ASM}\t${TOTAL}" | awk '{print $0"\t"((100*$3)/$4)}' >> completeness.stats
+  rm -r $asm.solid.meryl
+  echo
 
-	echo "# Generate ${asm}_only.tdf"
-	if [[ ! -e "$asm_fa.fai" ]]; then
-		echo "# Index $asm_fa"
-		samtools faidx $asm_fa
-		echo
-	fi
+  echo "# Generate ${asm}_only.tdf"
+  if [[ ! -e "$asm_fa.fai" ]]; then
+    echo "# Index $asm_fa"
+    samtools faidx $asm_fa
+    echo
+  fi
 
-	if [[ ! -e "${asm}_only.tdf" ]]; then
-		meryl-lookup -dump -memory 6 -sequence $asm_fa -mers ${asm}.0.meryl | awk '$(NF-4)=="T" {print $1"\t"$(NF-5)"\t"($(NF-5)+21)}' > ${asm}_only.bed
-		igvtools count ${asm}_only.bed ${asm}_only.tdf $asm_fa.fai
-		echo "${asm}_only.tdf generated."
-	else
-		echo
-		echo "*** ${asm}_only.tdf found. ***"
-	fi
-	echo
+  if [[ ! -e "${asm}_only.tdf" ]]; then
+    meryl-lookup -dump -memory 6 -sequence $asm_fa -mers ${asm}.0.meryl | awk '$(NF-4)=="T" {print $1"\t"$(NF-5)"\t"($(NF-5)+21)}' > ${asm}_only.bed
+    igvtools count ${asm}_only.bed ${asm}_only.tdf $asm_fa.fai
+    echo "${asm}_only.tdf generated."
+  else
+    echo
+    echo "*** ${asm}_only.tdf found. ***"
+  fi
+  echo
 done
 
 hist_asm_dist_only=$name.dist_only.hist
 if [[ "$asm2_fa" = "" ]]; then
-	echo "No asm2_fa given. Done."
+  echo "No asm2_fa given. Done."
 
-	hist=$name.spectra-asm.hist
+  hist=$name.spectra-asm.hist
 
-	if [[ -s $hist ]]; then
-		echo "*** Found $hist ***"
+  if [[ -s $hist ]]; then
+    echo "*** Found $hist ***"
 	else
-		echo "# $asm1 only"
-		meryl intersect output read.k$k.$asm1.meryl $read ${asm1}.meryl
+    echo "# $asm1 only"
+    meryl intersect output read.k$k.$asm1.meryl $read ${asm1}.meryl
 
-		echo "# Write output"
-		echo -e "Assembly\tkmer_multiplicity\tCount" > $hist
-		meryl histogram read.k$k.$asm1.0.meryl | awk '{print "read-only\t"$0}' >> $hist
-		meryl histogram read.k$k.$asm1.meryl | awk -v hap="${asm1}" '{print hap"\t"$0}' >> $hist
+    echo "# Write output"
+    echo -e "Assembly\tkmer_multiplicity\tCount" > $hist
+    meryl histogram read.k$k.$asm1.0.meryl | awk '{print "read-only\t"$0}' >> $hist
+    meryl histogram read.k$k.$asm1.meryl | awk -v hap="${asm1}" '{print hap"\t"$0}' >> $hist
 
-		echo "# Get asm only for spectra-asm"
-		ASM1_ONLY=`meryl statistics ${asm1}.0.meryl | head -n3 | tail -n1 | awk '{print $2}'`
-		echo -e "${asm1}\t0\t$ASM1_ONLY" > $hist_asm_dist_only
-	fi
+    echo "# Get asm only for spectra-asm"
+    ASM1_ONLY=`meryl statistics ${asm1}.0.meryl | head -n3 | tail -n1 | awk '{print $2}'`
+    echo -e "${asm1}\t0\t$ASM1_ONLY" > $hist_asm_dist_only
+  fi
 
-	echo "#	Plot $hist"
-	echo "\
-	Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.spectra-asm -z $hist_asm_dist_only"
-	Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.spectra-asm -z $hist_asm_dist_only
-	echo
+  echo "#	Plot $hist"
+  echo "\
+  Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.spectra-asm -z $hist_asm_dist_only"
+  Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.spectra-asm -z $hist_asm_dist_only
+  echo
 
-	echo "# Clean up"
-	rm -r ${asm1}.0.meryl read.k$k.$asm1.0.meryl read.k$k.$asm1.meryl $read_solid
-	echo "Done!"
+  echo "# Clean up"
+  rm -r ${asm1}.0.meryl read.k$k.$asm1.0.meryl read.k$k.$asm1.meryl $read_solid
+  echo "Done!"
 
-	exit 0
+  exit 0
 fi
 asm2=`echo $asm2_fa | sed 's/.fasta$//g' | sed 's/.fa$//g' | sed 's/.fasta.gz$//g' | sed 's/.fa.gz$//g'`
 rm -r read.k$k.$asm1.0.meryl read.k$k.$asm2.0.meryl
@@ -221,26 +221,26 @@ echo "\"Is my diploid assembly having k-mers in expected copy numbers?\""
 echo
 
 if [[ -s $hist ]]; then
-	echo
-	echo "*** $hist found. Skip k-mer counting per copy numbers ***"
-	echo
+  echo
+  echo "*** $hist found. Skip k-mer counting per copy numbers ***"
+  echo
 else
-	echo -e "Copies\tkmer_multiplicity\tCount" > $hist
-	meryl histogram read.k$k.0.meryl | awk '{print "read-only\t"$0}' >> $hist
-	for i in $(seq 1 4)
-	do
-	    echo "Copy = 1 .."
-	    meryl intersect output read.k$k.$i.meryl $read [ equal-to $i ${asm1}_${asm2}_union.meryl ]
-	    meryl histogram read.k$k.$i.meryl | awk -v cn=$i '{print cn"\t"$0}' >> $hist
-	    rm -r read.k$k.$i.meryl
-	    echo
-	done
+  echo -e "Copies\tkmer_multiplicity\tCount" > $hist
+  meryl histogram read.k$k.0.meryl | awk '{print "read-only\t"$0}' >> $hist
+  for i in $(seq 1 4)
+  do
+    echo "Copy = 1 .."
+    meryl intersect output read.k$k.$i.meryl $read [ equal-to $i ${asm1}_${asm2}_union.meryl ]
+    meryl histogram read.k$k.$i.meryl | awk -v cn=$i '{print cn"\t"$0}' >> $hist
+    rm -r read.k$k.$i.meryl
+    echo
+  done
 
-	echo "Copy >4 .."
-	meryl intersect output read.k$k.gt$i.meryl $read [ greater-than $i ${asm1}_${asm2}_union.meryl ]
-	meryl histogram read.k$k.gt$i.meryl | awk -v cn=">$i" '{print cn"\t"$0}' >> $hist
-	rm -r read.k$k.gt$i.meryl
-	echo
+  echo "Copy >4 .."
+  meryl intersect output read.k$k.gt$i.meryl $read [ greater-than $i ${asm1}_${asm2}_union.meryl ]
+  meryl histogram read.k$k.gt$i.meryl | awk -v cn=">$i" '{print cn"\t"$0}' >> $hist
+  rm -r read.k$k.gt$i.meryl
+  echo
 fi
 
 echo "# Count k-mers only seen in the assemblies, not in the reads"
@@ -290,38 +290,40 @@ echo "\"Is the assembled distinct portion bigger in one of the two assemblies?\"
 hist=$name.spectra-asm.hist
 
 if [[ -e $hist ]]; then
-	echo
-	echo "*** Found $hist. Skip re-computing. ***"
-	echo
+  echo
+  echo "*** Found $hist. Skip re-computing. ***"
+  echo
 else
-	echo "# Get ${asm1} / ${asm2} / shared kmers"
-	meryl difference output ${asm2}_only.meryl ${asm2}.meryl ${asm1}.meryl
-	meryl difference output ${asm1}_only.meryl ${asm1}.meryl ${asm2}.meryl
-	meryl intersect output ${asm1}_shrd.meryl ${asm1}.meryl ${asm2}.meryl
-	echo
+  echo "# Get ${asm1} / ${asm2} / shared kmers"
+  meryl difference output ${asm2}_only.meryl ${asm2}.meryl ${asm1}.meryl
+  meryl difference output ${asm1}_only.meryl ${asm1}.meryl ${asm2}.meryl
+  meryl intersect output ${asm1}_shrd.meryl ${asm1}.meryl ${asm2}.meryl
+  echo
 
-	echo "# $asm1 only"
-	meryl intersect output read.k$k.$asm1.meryl $read ${asm1}_only.meryl
-	echo
+  echo "# $asm1 only"
+  meryl intersect output read.k$k.$asm1.meryl $read ${asm1}_only.meryl
+  echo
 
-	echo "# $asm2 only"
-	meryl intersect output read.k$k.$asm2.meryl $read ${asm2}_only.meryl
-	echo
+  echo "# $asm2 only"
+  meryl intersect output read.k$k.$asm2.meryl $read ${asm2}_only.meryl
+  echo
 
-	echo "# shared ($asm1 and $asm2)"
-	meryl intersect output read.k$k.shrd.meryl $read ${asm1}_shrd.meryl
-	echo
+  echo "# shared ($asm1 and $asm2)"
+  meryl intersect output read.k$k.shrd.meryl $read ${asm1}_shrd.meryl
+  echo
 
-	echo "# Write output"
-	echo -e "Assembly\tkmer_multiplicity\tCount" > $hist
-	meryl histogram read.k$k.0.meryl | awk '{print "read-only\t"$0}' >> $hist
-	meryl histogram read.k$k.$asm1.meryl | awk -v hap="$asm1-only" '{print hap"\t"$0}' >> $hist
-	meryl histogram read.k$k.$asm2.meryl | awk -v hap="$asm2-only" '{print hap"\t"$0}' >> $hist
-	meryl histogram read.k$k.shrd.meryl | awk -v hap="shared" '{print hap"\t"$0}' >> $hist
-	echo
+  echo "# Write output"
+  echo -e "Assembly\tkmer_multiplicity\tCount" > $hist
+  meryl histogram read.k$k.0.meryl | awk '{print "read-only\t"$0}' >> $hist
+  meryl histogram read.k$k.$asm1.meryl | awk -v hap="$asm1-only" '{print hap"\t"$0}' >> $hist
+  meryl histogram read.k$k.$asm2.meryl | awk -v hap="$asm2-only" '{print hap"\t"$0}' >> $hist
+  meryl histogram read.k$k.shrd.meryl | awk -v hap="shared" '{print hap"\t"$0}' >> $hist
+  echo
 fi
 
 echo "Plot $hist"
+echo "\
+Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.spectra-asm -z $hist_asm_dist_only"
 Rscript $MERQURY/plot/plot_spectra_cn.R -f $hist -o $name.spectra-asm -z $hist_asm_dist_only
 echo
 
