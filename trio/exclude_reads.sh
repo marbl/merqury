f1fdc5cf56677f6be4d476aa82bc0d29357dcca9
arangrhie <arrhie@gmail.com>
2022-01-26 17:48:01 -0500

    support -10x mode for excluding reads containing mers

--- trio/exclude_reads.sh
+++ trio/exclude_reads.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 if [[ $# -lt 3 ]]; then
-	echo "Usage: ./exclude_reads.sh <in.meryl> <input.map> <out.prefix> [num_line]"
+	echo "Usage: ./exclude_reads.sh <in.meryl> <input.map> <out.prefix> [-10x] [num_line]"
 	echo "When a read has a k-mer in <in.meryl>, both pairs will be excluded."
 	echo -e "\t<in.meryl>   : meryl db to lookup"
 	echo -e "\t<input.map>  : <R1.fastq.gz> <tab> <R2.fastq.gz>"
 	echo -e "\t<out.prefix> : <out.prefix>.R1.fastq.gz and <out.prefix>.R2.fastq.gz will be generated."
+  echo -e "\t[-10x]       : set this if the first 23 bp in every R1 should be ignored."
   echo -e "\t[num_line]   : n'th line of the input.map will be processed."
 	exit -1
 fi
@@ -13,10 +14,19 @@ fi
 meryl=$1
 input=$2
 out=$3
+opt=$4
 
-if ! [[ -z $4 ]]; then
+if [[ "$opt" == "-10x" ]]; then
+  i=$5
+fi
+
+
+if [[ "$opt" != "-10x" ]]; then
+  opt=""
   i=$4
-else
+fi
+
+if [[ -z $i ]]; then
   i=$SLURM_ARRAY_TASK_ID
 fi
 
@@ -35,8 +45,8 @@ if [[ -z $read2 ]]; then
 fi
 
 echo "
-meryl-lookup -exclude -sequence $read1 $read2 -mers $meryl -output ${out_1} ${out_2}
+meryl-lookup -exclude $opt -sequence $read1 $read2 -mers $meryl -output ${out_1} ${out_2}
 "
-meryl-lookup -exclude -sequence $read1 $read2 -mers $meryl -output ${out_1} ${out_2}
+meryl-lookup -exclude $opt -sequence $read1 $read2 -mers $meryl -output ${out_1} ${out_2}
 
 
